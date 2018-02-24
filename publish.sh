3d021fbab25ac6b58a7d981a7a684904dc6c0882
Xiaozh <838223935@qq.com>
2018-02-24 17:05:25 +0800

    fea: CI support and deploy script

--- publish.sh
+++ publish.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+proj_path=$GOPATH/src/github.com/hzxiao/ocss-srv
+app_path=/home/jks/app
+proj_name=ocss
+
+cd $proj_path
+git checkout .
+git pull
+
+sudo chmod +x ./pkg.sh
+./pkg.sh
+
+cp build/$proj_name'.zip' $app_path
+cd $app_path
+
+unzip $proj_name'.zip'
+
+sudo /etc/rc.d/init.d/$proj_name'd' restart
